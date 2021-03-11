0733d0c3a98cabf5b54745208626f980611cce3f
Luke Wyatt <luke.a.wyatt@gmail.com>
2021-03-11 15:53:14 -0500

    Initial Revision
    Initial Revision

---
+++ BackupAzureTableStorageLocally/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using TheByteStuff.AzureTableUtilities;
+
+namespace BackupAzureToLocal
+{
+
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("AZURE TABLE BACKUPS");
+            Console.WriteLine("-------------------");
+            Console.WriteLine();
+
+            var argsProperlyFormatted = true;
+
+            if (args.Length == 0
+                || string.IsNullOrEmpty(args[0])
+                || args[0].Trim() == "help"
+                || args[0].Trim() == "-help"
+                || args[0].Trim() == "--help"
+                || args[0].Trim() == "h" 
+                || args[0].Trim() == "-h" 
+                || args[0].Trim() == "--h"
+                || args[0].Trim() == "?"
+                || args[0].Trim() == "-?"
+                || args[0].Trim() == "--?"
+                )
+                    argsProperlyFormatted = false;
+
+            if (argsProperlyFormatted && string.IsNullOrEmpty(args[1]))
+                argsProperlyFormatted = false;
+
+            if (argsProperlyFormatted && string.IsNullOrEmpty(args[2]))
+                argsProperlyFormatted = false;
+
+            if (argsProperlyFormatted && args[2].Split('|', StringSplitOptions.RemoveEmptyEntries).Length == 0)
+                    argsProperlyFormatted = false;
+
+            if (!argsProperlyFormatted)
+            {
+                Console.WriteLine("PROPER USAGE:");
+                Console.WriteLine("BackupAzureToLocal.exe \"{A1}\" \"{A2}\" \"{A3}\"");
+                Console.WriteLine("A1: Azure Storage Connection String");
+                Console.WriteLine("A2: Local Backup Directory");
+                Console.WriteLine("A3: Table Names, Delimited by pipes ('|')");
+                Console.WriteLine();
+                return;
+            }
+
+            string azureStorageConfigConnection = args[0];
+            string backupDirectory = args[1];
+            string[] tables = args[2].Split('|', StringSplitOptions.RemoveEmptyEntries);
+
+            BackupAzureTables backup = new BackupAzureTables(azureStorageConfigConnection);
+            foreach (var t in tables)
+            {
+                Console.WriteLine("BACKING UP AZURE TABLE: " + t);
+
+                string resp = backup.BackupTableToFile(
+                    t,
+                    backupDirectory,
+                    false, 
+                    true,
+                    600
+                );
+
+                Console.WriteLine("RESULT: " + resp);
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("FINISHED JOB");
+        }
+
+    }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # BackupAzureTableStorageLocally
 
+A simple console application written in .NET 5.0 to backup tables from within a single Azure storage account and store them locally.
+
+This project uses [TheByteStuff's AzureTableUtilites Nuget Package](https://www.nuget.org/packages/TheByteStuff.AzureTableUtilities/)
+
+## Usage
+
+Run the BackupAzureTableStorageLocally executable using the example below.
+
+```shell
+BackupAzureTableStorageLocally.exe "{A1}" "{A2}" "{A3}"
+
+# WHERE PARAMETERS EQUAL
+# A1: Azure Storage Connection String
+# A2: Local Backup Directory
+# A3: Table Names, Delimited by pipes ('|')
+```
